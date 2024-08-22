a6ec7e7539a49e2baab21599369fbb4f6b50fab5
João Colares <joaocolaresm@gmail.com>
2024-08-22 16:59:08 -0300

    aula 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
 import Banner from './components/Banner'
 import Header from './components/Header'
 import {GlobalCss} from './styles'
+
+const rotas = createBrowserRouter([
+  {
+    path: '/',
+    element: <Banner/>
+  }
+])
+
 function App() {
   return (
     <>
@@ -8,7 +18,7 @@ function App() {
       <div className='container'>
         <Header/>
       </div>
-      <Banner/>
+      <RouterProvider router={rotas}/>
     </>
   )
 }
